bfa60484509b461b60de639c12333e3d75766716
Loi Tran <loitran454@gmail.com>
2023-08-26 14:39:50 -0700

    added functions

--- src/HandleInput.java
+++ src/HandleInput.java
@@ -1,13 +1,41 @@
-package tictactoe;
-
 import java.util.Scanner;
 
 public class HandleInput {
-    Scanner scn = new Scanner(System.in);
 
-    public HandleInput() {
+    public HandleInput(Scanner scn) {
 
     }
+
+    protected String getInputString() {
+        Scanner scn = new Scanner(System.in);
+        return scn.nextLine();
+    }
+
+    protected char[] arrayFromInput() {
+        String input = getInputString();
+        return input.toCharArray();
+    }
+
+    protected char[][] multiArrayFromInput() {
+
+        String symbols = getInputString();
+
+        int row = 3;
+        int col = 3;
+
+        char[][] board = new char[row][col];
+
+        // add the symbols onto the 3x3 board
+        int count = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                board[i][j] = symbols.charAt(count++);
+
+            }
+        }
+        return board;
+    }
+
 
 
 }
--- src/Symbol.java
+++ src/Symbol.java
@@ -1,105 +1,156 @@
-import java.util.Scanner;
-
 public class Symbol {
 
-    Scanner scn = new Scanner(System.in);
+    HandleInput input;
 
     public Symbol() {
-        printState();
-    }
-
-    protected void printState() {
-        printGrid();
-        analyzeGameState();
+        this.input = new HandleInput();
     }
 
     // print 3x3 grid of the numbers
     protected void printGrid() {
-//        String[] symbols = scn.nextLine().split("");
-//
-//        int grid = 3;
-//        System.out.println("---------");
-//        for (int i = 0; i < symbols.length; i++) {
-//
-//            // beginning of line
-//            if (i % grid == 0) {
-//                System.out.print("| ");
-//            }
-//            System.out.print(symbols[i] + " ");
-//
-//            // end of line
-//            if (i % grid == 2) {
-//                System.out.println("|");
-//            }
-//        }
-//        System.out.println("---------");
-        System.out.println("---------");
 
-        for (char[] r : arrayFromInput()) {
+        char[][] array = input.multiArrayFromInput();
+
+        System.out.println("---------");
+        for (char[] r : array) {
             System.out.print("| ");
             for (char c : r) {
                 System.out.print(c + " ");
             }
             System.out.println("|");
         }
-
         System.out.println("---------");
 
+
+//        analyzeSymbol('O');
         analyzeGameState();
-    }
-
-    // return array of char[][] from the input entered
-    protected char[][] arrayFromInput() {
-
-        String symbols = scn.nextLine();
-
-        int row = 3;
-        int col = 3;
-
-        char[][] board = new char[row][col];
-
-        // add the symbols onto the 3x3 board
-        int count = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                board[i][j] = symbols.charAt(count++);
-
-            }
-        }
-        return board;
     }
 
 
     // check winning conditions
-    // print out the game state:
-    // - Game not finished
-    // - Draw
-    // - X wins
-    // - O wins
-    // - Impossible
     protected void analyzeGameState() {
-        // check left diagonal
 
-        // check right diagonal
+        char xSymbol = 'X';
+        char oSymbol = 'O';
 
-        // check by row
+        boolean xIsWinning = isWinning(xSymbol);
+        boolean oIsWinning = isWinning(oSymbol);
 
-        // check by col
+        int numberOfX = getNumberOfSymbols('X');
+        int numberOfO = getNumberOfSymbols('O');
 
-        // check for impossibles
-        // X or O difference is 2 or more, 3 X's and 3 O's
-        // neither side has 3 in a row -- grid still has empty cells
-        char[][] grid = arrayFromInput();
-        boolean XTurn = true;
+        // if difference is 2 or more
+        int diffX = numberOfX - numberOfO;
+        int diffO = numberOfO - numberOfX;
 
-        for (int row = 0; row < 3; row++) {
-
-            for (int col = 0; col < 3; col++) {
-
-            }
-
+        if (!xIsWinning && !oIsWinning && getNumberOfSymbols('_') == 0) {
+            System.out.println("Draw.");
+        } else if (!xIsWinning && !oIsWinning && getNumberOfSymbols('_') > 0) {
+            System.out.println("Game not finished");
+        } else if (xIsWinning && oIsWinning || diffX >= 2 || diffO >= 2) {
+            System.out.println("Impossible.");
         }
 
 
+
     }
+
+    protected int getNumberOfSymbols(char symbol) {
+        char[] symbols = input.arrayFromInput();
+
+        int count = 0;
+
+        for (char s : symbols) {
+            if (s == symbol) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // check if a certain letter won
+    protected boolean isWinning(char letter) {
+
+        char[] symbols = input.arrayFromInput();
+
+        int winNum = 3;
+
+        int colOne = 0;
+        int colTwo = 0;
+        int colThree = 0;
+
+        int rowOne = 0;
+        int rowTwo = 0;
+        int rowThree = 0;
+
+        int diagonalOne = 0;
+        int diagonalTwo = 0;
+
+        boolean winning = false;
+
+        for (int i = 0; i < symbols.length; i++) {
+            // check vertical
+            if (symbols[i] == letter) {
+                // check vertical
+                if (i % winNum == 0) {
+                    colOne++;
+                }
+                if (i % winNum == 1) {
+                    colTwo++;
+                }
+                if (i % winNum == 2) {
+                    colThree++;
+                }
+                // check horizontal
+                if (i < winNum) {
+                    rowOne++;
+                } else if (i < 6) {
+                    rowTwo++;
+                } else if (i < 9) {
+                    rowThree++;
+                }
+                // check top left to bottom right diagonal
+                if (i % 4 == 0) {
+                    diagonalOne++;
+                }
+                // check top right to bottom left diagonal
+                if (i % 2 == 0 && i <= 6 && i != 0) {
+                    diagonalTwo++;
+                }
+            }
+        }
+
+        // check 8 winning conditions here:
+        if (colOne == winNum || colTwo == winNum || colThree == winNum) {
+            winning = true;
+        } else if (rowOne == winNum || rowTwo == winNum || rowThree == winNum) {
+            winning = true;
+        } else if (diagonalOne == winNum || diagonalTwo == winNum) {
+            winning = true;
+        }
+
+        return winning;
+
+        // check if it's not winning...
+        // draw or game not finished
+
+//        System.out.println("col1 x count: " + colOne);
+//        System.out.println("col2 x count: " + colTwo);
+//        System.out.println("col3 x count: " + colThree + "\n");
+//
+//        System.out.println("row1 x count: " + rowOne);
+//        System.out.println("row2 x count: " + rowTwo);
+//        System.out.println("row3 x count: " + rowThree + "\n");
+//
+//        System.out.println("diagonal1 x count: " + diagonalOne);
+//        System.out.println("diagonal2 x count: " + diagonalTwo + "\n");
+//
+//        System.out.println("letter count: " + letterCounter);
+
+
+    }
+
+
 }
+
+
